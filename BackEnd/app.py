af26310140cdf1c5e1467db52d97138c8b9c1c0a
Noe Horowitz <noehoro@gmail.com>
2020-11-07 15:28:16 -0500

    fix flask

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,14 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData, create_engine, text
-from random import randint
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 CORS(app)
 
-CONN = create_engine('sqlite:///database.db')
+# CONN = create_engine('sqlite:///database.db')
 
 META_DATA = MetaData(bind=CONN, reflect=True)
 
