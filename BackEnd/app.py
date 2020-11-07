83024fb29cf5dcf42e2b418ff4a6af826b67b2cc
Noe Horowitz <noehoro@gmail.com>
2020-11-07 15:31:39 -0500

    flask fix 2.0

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -4,11 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# CONN = create_engine('sqlite:///database.db')
-
-META_DATA = MetaData(bind=CONN, reflect=True)
-
-META_DATA.reflect()
+@app.route('/')
+def homepage():
+    return '<center><img src="https://static.wikia.nocookie.net/f377126c-7717-4026-aa5b-7ca887157442" alt="Hello There!"><br/><img src="https://media.makeameme.org/created/general-kenobi-5b18a9.jpg" alt="General Kenobi!"></center>'
 
 
 if __name__ == "__main__":
