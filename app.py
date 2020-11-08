9c16f976ceebd4581c5a8079d595b812979ab91c
2020wmarvil <2020wmarvil@gmail.com>
2020-11-07 20:40:04 -0500

    Add QRcode generation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import qrcode
 
 # from config import API_KEY, DB_URL
 
@@ -27,11 +28,27 @@ CORS(app)
 def homepage():
     return '<center><img src="https://static.wikia.nocookie.net/f377126c-7717-4026-aa5b-7ca887157442" alt="Hello There!"><br/><img src="https://media.makeameme.org/created/general-kenobi-5b18a9.jpg" alt="General Kenobi!"></center>'
 
-
-@app.route('/createline', methods=['POST'])
+@app.route('/createline', methods=['GET'])
 def createLine():
+    url = "https://next-in-line-rpi.herokuapp.com/"
+
+    qr = GenerateQRCode(url)
+    qr.save("code.png") 
 
     return jsonify({"code": 123})
+
+def GenerateQRCode(url):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_M,
+        box_size=10,
+        border=4
+    )
+    
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    return qr.make_image(fill_color="black", back_color="white")
 
 
 if __name__ == "__main__":
