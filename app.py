74728483b7e58ceb283e930e26e9650a00f5ed7a
Noe Horowitz <noehoro@gmail.com>
2020-11-07 23:44:42 -0500

    comments

--- app.py
+++ app.py
@@ -40,15 +40,30 @@ def homepage():
 # individually creates a customer instance
 # Kevin made it. Ask Kevin for questions
 
+# Peron sees QR code,
+# Scan QR
+# QR code = website.com/#/scanned?code=1234
+# React:
+# First look at code and remember it,
+# form asking for name and phone number,
+# On submit, do post request to '/join' with request schema of {name: name, phone: phone, code:code}
+# Flask:
+# Create Customer
+# add customer to list
+# done
 
-@app.route('/addCustomer')
-def addCustomer():
-    name = request.args.get('name', None)
-    phone = request.args.get('phone', None)
+
+@app.route('/join', methods=['POST'])
+def join():
+    name = request.get_json()['name']
+    phone = request.get_json()['phone']
+    code = request.get_json()['code']
+
     customer_doc = Document(
         name=name,
         phone=phone)
 
+    # look through all lines, with the code
     response = instance.post_document(
         db='customer', document=customer_doc).get_result()
 
@@ -70,11 +85,16 @@ def getLines():
     return response
 
 
+# Barber opens shop
+# creates List, enters name
+# Get name and code
+# create new empty line, and also generates the code
+# returns the code
+
 @app.route('/createline', methods=['POST'])
 def createLine():
-    code = random.randint(1000, 9999)
+    code = random.randint(100000, 999999)
     name = request.get_json()['name']
-    session['line_operator'] = {"name": name, "code": code}
 
     line_doc = Document(
         code=code,
@@ -83,6 +103,8 @@ def createLine():
 
     response = instance.post_document(
         db='line', document=line_doc).get_result()
+
+    session['line_operator'] = {"name": name, "code": code}
 
     return jsonify({"code": code})
 
@@ -95,7 +117,6 @@ def join(code):
 
 # @app.route('/getlist/<code>')
 # def getList(code):
-
 
 
 def GenerateQRCode(url):
