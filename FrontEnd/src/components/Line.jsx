ff73a953a6d7cb6c103173f679d4b09197ea2981
Noe Horowitz <noehoro@gmail.com>
2020-11-07 22:42:42 -0500

    QR code stuff react

--- FrontEnd/src/components/Line.jsx
+++ FrontEnd/src/components/Line.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { Button } from "react-bootstrap";
 
 export const Line = () => {
@@ -16,14 +16,19 @@ export const Line = () => {
   return (
     <div className="container w-50">
       <div>Your line's name is {localStorage.getItem("lineName")}</div>
-      <img alt="QR CODe" src={createQRCode()} />
+      <img alt="QR CODE" src={createQRCode()} />
 
       <div className="mb-5">Your line's code is: {name}</div>
-      <div className="mb-5">QR CODE:</div>
-      <div className="row">
+      <div className="row mb-5">
         <div className="col-sm-4"></div>
         <Button className="col-sm-4" variant="success" onClick={buttonClick}>
           See Line
+        </Button>
+      </div>
+      <div className="row">
+        <div className="col-sm-4"></div>
+        <Button className="col-sm-4" variant="success" onClick={buttonClick}>
+          Ready for New Customer
         </Button>
       </div>
     </div>
